6a821c65aaf8fb1e086b77963f32bc05b70e2973
piripopero <45849251+piripopero@users.noreply.github.com>
2019-06-16 21:26:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-    echo 'Hola Mundo';
-?>
+<HTML>
+    <HEAD>
+        <TITLE>HK</TITLE>
+    </HEAD>
+
+    <BODY>
+        <img src="titurf.jpg"
+        alt="La cabeza y el torso de un esqueleto de dinosaurio;
+              tiene una cabeza grande con dientes largos y filosos"
+        width="700"
+        height="341"
+        title="Un T-Rex en exhibición en el Museo de la Universidad de Manchester.">
+    </BODY>
+</HTML>
